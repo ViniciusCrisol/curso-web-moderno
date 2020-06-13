36be026422b9d00b851c428a8a82858d976f593f
Vinicius Crisol <61323173+ViniciusCrisol@users.noreply.github.com>
2020-06-13 09:07:35 -0300

    Template String

---
+++ exercicios-js/fundamentos/string.js
@@ -0,0 +1,5 @@
+const escola = 'cod3r';
+
+console.log(escola + '!');
+console.log('3' + 2);
+console.log('Ana,MAria,Pedro'.split(','));
---
+++ exercicios-js/fundamentos/templateString.js
@@ -0,0 +1,12 @@
+const nome = 'Rebeca';
+
+console.log(`
+  Olá
+  ${nome}
+`);
+
+console.log(`1+1 = ${1 + 1}`);
+
+const up = (texto) => texto.toUpperCase();
+
+console.log(`Ei... ${up('cuidado')}`);
