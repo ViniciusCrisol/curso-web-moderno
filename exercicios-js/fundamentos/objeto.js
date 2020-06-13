87de1352fe1bdaebea265eda282d6c15036b7e33
Vinicius Crisol <61323173+ViniciusCrisol@users.noreply.github.com>
2020-06-13 09:18:47 -0300

    objeto

---
+++ exercicios-js/fundamentos/objeto.js
@@ -0,0 +1,16 @@
+const prod1 = {};
+prod1.name = 'Iphone 22';
+prod1.preco = 4999999;
+
+console.log(prdo1);
+
+const prod2 = {
+  nome: 'Camisa Polo',
+  preco: 71.1,
+  cores: {
+    cor1: 'rosa',
+    cor2: 'Azul',
+  },
+};
+
+console.log(prod2);
