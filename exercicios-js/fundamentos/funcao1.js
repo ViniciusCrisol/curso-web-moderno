4b0068419337bc9effb86b77f2a2d17139681b5b
Vinicius Crisol <61323173+ViniciusCrisol@users.noreply.github.com>
2020-06-13 09:40:14 -0300

    Function1

---
+++ exercicios-js/fundamentos/funcao1.js
@@ -0,0 +1,16 @@
+// Função sem retorno
+
+function imprimirSoma(a, b) {
+  console.log(a + b);
+}
+
+imprimirSoma(2, 3);
+
+// Função com retorno
+
+function soma(a, b = 0) {
+  // Se não passar o 2 parâmetro ele irá veler 0
+  return a + b;
+}
+
+console.log(2, 3);
